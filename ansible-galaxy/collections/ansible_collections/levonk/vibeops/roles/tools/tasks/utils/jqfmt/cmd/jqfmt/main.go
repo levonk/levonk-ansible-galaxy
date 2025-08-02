765e23091f6fc9080a3c8ba4121db3bf822e972b
levonk <v3l8dud3@lkara.com>
2025-08-01 17:04:23 -0700

    fix(submodules): convert jqfmt from submodule to regular files
    - Remove jqfmt git submodule reference that was causing test failures
    - Add jqfmt files directly to the repository
    - This resolves the 'no submodule mapping found' error during test execution

---
+++ ansible-galaxy/collections/ansible_collections/levonk/vibeops/roles/tools/tasks/utils/jqfmt/cmd/jqfmt/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/noperator/jqfmt"
+	log "github.com/sirupsen/logrus"
+)
+
+func assertErrorToNilf(message string, err error) {
+	if err != nil {
+		log.Fatalf(message, err)
+	}
+}
+
+func main() {
+	// funcsStr := flag.String("fn", "", "functions")
+	opsStr := flag.String("op", "", "operators")
+	obj := flag.Bool("ob", false, "objects")
+	arr := flag.Bool("ar", false, "arrays")
+	oneLn := flag.Bool("o", false, "one line")
+	file := flag.String("f", "", "file")
+	verbose := flag.Bool("v", false, "verbose")
+	// noHang := flag.Bool("nh", false, "no hanging indent")
+	flag.Parse()
+	var from_stdin bool = false
+
+	if *verbose {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	if *file == "" {
+		from_stdin = true
+	}
+
+	// var funcs []string
+	// if *funcsStr == "" {
+	// 	funcs = []string{}
+	// } else {
+	// 	funcs = strings.Split(*funcsStr, ",")
+	// }
+
+	var ops []string
+	if *opsStr == "" {
+		ops = []string{}
+	} else {
+		ops = strings.Split(*opsStr, ",")
+	}
+
+	cliCfg, err := jqfmt.ValidateConfig(jqfmt.JqFmtCfg{
+		Arr: *arr,
+		// Funcs: funcs,
+		Obj:   *obj,
+		OneLn: *oneLn,
+		Ops:   ops,
+		// Hang:  !(*noHang),
+	})
+	assertErrorToNilf("invalid config: %v", err)
+
+	var jqBytes []byte
+	if from_stdin {
+		jqBytes, err = io.ReadAll(os.Stdin)
+	} else {
+		jqBytes, err = os.ReadFile(*file)
+	}
+	assertErrorToNilf("could not read file: %v", err)
+	jqStr := string(jqBytes)
+	jqStrFmt, err := jqfmt.DoThing(jqStr, cliCfg)
+	assertErrorToNilf("could not format jq: %v", err)
+	fmt.Print(jqStrFmt)
+}
